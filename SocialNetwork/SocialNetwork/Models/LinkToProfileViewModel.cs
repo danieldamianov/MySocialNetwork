4e79facd53a9e9fba97afeeef6fcc3eab2c13cf1
danieldamianov <danieldamianov02@gmail.com>
2020-03-04 20:16:06 +0200

    Added partial view for profile picture link.

---
+++ SocialNetwork/SocialNetwork/Models/LinkToProfileViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SocialNetwork.Models
+{
+    public class LinkToProfileViewModel
+    {
+        public LinkToProfileViewModel(string userId, string avatarCode, string username, int avatarSize)
+        {
+            UserId = userId;
+            AvatarCode = avatarCode;
+            Username = username;
+            AvatarSize = avatarSize;
+        }
+
+        public string UserId { get; set; }
+
+        public string AvatarCode { get; set; }
+
+        public string Username { get; set; }
+
+        public int AvatarSize { get; set; }
+    }
+}
