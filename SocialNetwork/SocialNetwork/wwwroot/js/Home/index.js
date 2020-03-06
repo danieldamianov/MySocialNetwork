6f3618db0377a7fe4171d215c5fea3654b4075ae
danieldamianov <danieldamianov02@gmail.com>
2020-03-06 20:09:22 +0200

    Improved the styling of the app.

--- SocialNetwork/SocialNetwork/wwwroot/js/Home/index.js
+++ SocialNetwork/SocialNetwork/wwwroot/js/Home/index.js
@@ -2,7 +2,6 @@
     jQuery.get("Likes/LikePost?userWhoLikesIt=" + userId + "&likedPostId=" + postId)
         .done(function success(data) {
             if (data) {
-                alert('dsa')
                 var firstLikes = parseInt(document.getElementById(postId + 'likesCount').innerHTML);
                 var afterLiking = ++firstLikes;
                 document.getElementById(postId + 'likesCount').innerHTML = afterLiking;
